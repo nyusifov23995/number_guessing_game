a4a2daf7e14da6bf566db31dca9b94e9ab29d765
Nurlan Yusifov <nurlan.datapath@gmail.com>
2025-10-17 03:46:08 +0400

    Update number_guessing_game.py

---
+++ number_guessing_game.py
@@ -0,0 +1,23 @@
+import random
+num=random.randint(1,50)
+guesses=[]
+for attempt in range(5):
+    a=int(input("Guess a number between 0 and 51: "))
+    guesses.append(a)
+    if a==num:
+        if (attempt+1)==1:
+            print(f"Correct!You guessed the number correctly on your {attempt+1}st try!")
+        elif(attempt+1)==2:
+            print(f"Correct!You guessed the number correctly on your {attempt+1}nd try!")
+        elif (attempt+1)==3:
+            print(f"Correct!You guessed the number correctly on your {attempt+1}rd try!")
+        else:
+            print(f"Correct!You guessed the number correctly on your {attempt+1}th try!")
+        break
+    elif a>num:
+        print("Too high")
+    else:
+        print("Too low")
+else:
+    print("Sorry! The correct number was {}".format(num))
+    print(f"Your guesses were: {guesses}")
